6d6689dc006327d2d5db2a3d32f8f8e35c83739e
RaymondLin <raymond.lin0226@gmail.com>
2018-12-21 23:23:12 -0800

    finished projects section

--- scripts.js
+++ scripts.js
@@ -1,12 +1,17 @@
-function readmore_toggle() {
-	var button = document.getElementById(readmore_but);
+function read_more(inbutton, description) {
+	var button = document.getElementById(inbutton);
+	var text = document.getElementById(description);
 
 	if(button.innerHTML == "Read More")
 	{
-		
+		button.innerHTML = "Read Less";
+		text.style.display = "auto";
+		text.style.height = "auto";
 	}
 	else
 	{
-
+		button.innerHTML = "Read More";
+		text.style.display = "block";
+		text.style.height = "200px";
 	}
 }
