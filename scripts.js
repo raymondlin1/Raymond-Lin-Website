2d5dd77ebf50eeec1d188247e3c441f769803172
RaymondLin <raymond.lin0226@gmail.com>
2019-01-06 01:46:48 -0800

    started working on modal images

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ function read_more(inbutton, description) {
 	if(button.innerHTML == "Read More")
 	{
 		button.innerHTML = "Read Less";
-		//text.style.display = "auto";
+		text.style.display = "auto";
 		text.style.height = "auto";
 	}
 	else
@@ -13,5 +13,23 @@ function read_more(inbutton, description) {
 		button.innerHTML = "Read More";
 		text.style.display = "block";
 		text.style.height = "230px";
+	}
+}
+
+
+function enlarge_image(in_modal, in_image, in_modal_image) {
+	var modal = document.getElementById(in_modal);
+	var image = document.getElementById(in_image);
+	var modal_image = document.getElementById(in_modal_image);
+
+	image.onclick = function() {
+		modal.style.display = "block";
+		modal_image.src = this.src;
+	}
+
+	var span = document.getElementsByClassName("close")[0];
+
+	span.onclick = function() {
+		modal.style.display = "none";
 	}
 }
